f26cc111c1f03405c38443ee9ea2df4dfb34de8e
ibo <ibo.b@tcs.com>
2016-06-01 15:00:06 -0400

    Added encryption password to AdminUser

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,6 +2,8 @@ class AdminUser < ActiveRecord::Base
   has_and_belongs_to_many :pages
   has_many :section_edits
   has_many :sections, :through => :section_edits
+  
+  has_secure_password
 
   EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\z/
   validates_presence_of :first_name
@@ -9,7 +11,7 @@ class AdminUser < ActiveRecord::Base
   validates_presence_of :last_name
   validates_length_of :last_name, :maximum => 50
   validates_presence_of :username
-  validates_length_of :username, :maximum => 8..25
+  validates_length_of :username, :within => 8..25
   validates_uniqueness_of :username
   validates_presence_of :email
   validates_length_of :email, :maximum => 100
